19bf0c37b495eb4a3cd8ea0446856d37450539ae
nicoSix <nicolasix@outlook.fr>
2021-08-09 00:56:03 +0200

    Adding shrinkable navbar when browser size is low

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -16,18 +21,29 @@ const useStyles = makeStyles((theme) => ({
       textDecoration: 'underline white',
     }
   },
+  drawerItem: {
+    padding: '10px',
+    textDecoration: 'none'
+  },
   title: {
     flexGrow: 1,
     textAlign: 'left',
     color: 'white',
     textDecoration: 'none'
   },
-  titleSize: {
+  titleChild: {
     fontSize: '120%'
+  },
+  titleDrawer: {
+    fontSize: '120%',
+    textAlign: 'center'
   },
   appBar: {
     backgroundColor: '#52307c',
-    marginBottom: '20px'
+    marginBottom: '20px',
+    "@media (max-width: 900px)": {
+      paddingLeft: 0,
+    },
   },
   downloadCV: {
     '&:hover': {
@@ -39,29 +55,104 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Navbar() {
   const classes = useStyles();
+  const [mobileView, setMobileView] = useState(window.innerWidth < 900);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
+  const handleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      setMobileView(window.innerWidth < 900);
+      if (window.innerWidth > 900) setDrawerOpen(false);
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    }
+  }, []);
+
+  const displayMobile = () => {
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick: handleDrawer,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawer,
+          }}
+        >
+          <Typography variant="overline" className={classes.titleDrawer}>
+            Menu
+          </Typography>
+          <Divider />
+          <Link className={classes.drawerItem} to="/experience">
+            Past experiences
+          </Link>
+          <Divider />
+          <Link className={classes.drawerItem} to="/publications">
+            Publications
+          </Link>
+          <Divider />
+          <Link className={classes.drawerItem} to="/projects">
+            Projects
+          </Link>
+          <Divider />
+          <span className={classes.drawerItem} onClick={() => window.open('/files/cv.pdf')}>
+            Download CV
+          </span>
+        </Drawer>
+        <Link to="/" className={classes.title}>
+          <Typography variant="overline" className={classes.titleChild}>
+            <b>Nicolas Six</b>
+          </Typography>
+        </Link>
+      </Toolbar>
+    );
+  };
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar>
+        <Link to="/" className={classes.title}>
+          <Typography variant="overline" className={classes.titleChild}>
+            <b>Nicolas Six</b>
+          </Typography>
+        </Link>
+        <Link to="/experience" className={classes.navbarItem}>
+          Past experiences
+        </Link>
+        <Link to="/publications" className={classes.navbarItem}>
+          Publications
+        </Link>
+        <Link to="/projects" className={classes.navbarItem}>
+          Projects
+        </Link>
+        <span className={classes.downloadCV} onClick={() => window.open('/files/cv.pdf')}>
+          Download CV
+        </span>
+      </Toolbar>
+    )
+  }
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Link to="/" className={classes.title}>
-            <Typography variant="overline" className={classes.titleSize}>
-              <b>Nicolas Six</b>
-            </Typography>
-          </Link>
-          <Link to="/experience" className={classes.navbarItem}>
-            Past experiences
-          </Link>
-          <Link to="/publications" className={classes.navbarItem}>
-            Publications
-          </Link>
-          <Link to="/projects" className={classes.navbarItem}>
-            Projects
-          </Link>
-          <span className={classes.downloadCV} onClick={() => window.open('/files/cv.pdf')}>
-            Download CV
-          </span>
-        </Toolbar>
+        { mobileView ? displayMobile() : displayDesktop() }
       </AppBar>
     </div>
   );
